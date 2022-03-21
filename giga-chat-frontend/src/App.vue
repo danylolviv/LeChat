8c90414605a702a2a53145d57ccf30cfd7ed4356
Danylo <danyloboshagora@gmail.com>
2022-03-21 03:45:17 +0100

    some styles

--- giga-chat-frontend/src/App.vue
+++ giga-chat-frontend/src/App.vue
@@ -6,14 +6,40 @@ const userStore = UserStore();
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/createUser">Create User</RouterLink> |
-    <RouterLink to="/room">Room</RouterLink>
-    <span v-if="userStore.userName.length > 0">
+  <div class="container">
+    <nav class="navbarContainer">
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/createUser">Create User</RouterLink> |
+      <RouterLink to="/room">Room</RouterLink>
+      <span v-if="userStore.userName.length > 0">
       | Logged in as: {{ userStore.userName }}</span
-    >
-  </nav>
-  <br />
-  <RouterView />
+      >
+    </nav>
+    <br />
+    <RouterView />
+  </div>
+
 </template>
+
+<style scoped>
+.container{
+  display: flex;
+  flex-direction: column;
+}
+.navbarContainer{
+  display: flex;
+  justify-content: center;
+  padding: 2%;
+  background: #181818;
+  color: white;
+}
+a{
+  color: hsla(160, 100%, 37%, 1);
+  text-decoration: none;
+}
+nav button {
+  padding: 1%;
+  margin-right: 3%;
+  background: #181818;
+}
+</style>
