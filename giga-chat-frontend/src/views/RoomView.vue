3fbff202e1de4e4465b6f1ddffdec4907aa18a70
Danylo <danyloboshagora@gmail.com>
2022-03-21 07:26:51 +0100

    Finally

--- giga-chat-frontend/src/views/RoomView.vue
+++ giga-chat-frontend/src/views/RoomView.vue
@@ -1,13 +1,17 @@
 <template>
-
+<div class="container">
   <input v-model="txtRoomListener" placeholder="Enter room name" /> <br />
-  <router-link v-bind:to="'/chat/' + txtRoomListener" @click="createRoom">Connect</router-link>
-  <ul>
-    <RouterLink v-bind:to="'/chat/' + room.name"  v-for="(room, index) in chatStore.listOfRooms"
-                @click="joinRoom(room.name)" v-bind:key="index">
-      {{ room.name }}
-    </RouterLink>
-  </ul>
+   <button class="createRoom">
+     <router-link v-bind:to="'/chat/' + txtRoomListener" @click="createRoom">Create room</router-link>
+   </button>
+  <div class="roomContainer" >
+    <RouterLink class="roomBtn" v-bind:to="'/chat/' + room.name"  v-for="(room, index) in chatStore.listOfRooms"
+                  @click="joinRoom(room.name)" v-bind:key="index">
+        <button class="but">{{ room.name }}</button>
+      </RouterLink>
+  </div>
+</div>
+
 </template>
 
 <script setup lang="ts">
@@ -27,4 +31,56 @@ function createRoom(){
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.createRoom{
+  background: #c5c5c5;
+  color: white;
+}
+.roomBtn{
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.but{
+  width: fit-content;
+  background: #4d4343;
+  border-color: hsla(160, 100%, 37%, 1);
+}
+
+.roomContainer{
+  width: 60%;
+  display: flex;
+  justify-content: flex-start;
+}
+
+.container{
+  border-radius: 15px;
+  padding: 2%;
+  height: auto;
+  min-height: 650px;
+  background: #181818;
+  color: white;
+}
+
+input{
+  width: 40%;
+  border-color: hsla(160, 100%, 37%, 1);
+  background: #2c3e50;
+  color: hsla(160, 100%, 37%, 1);
+}
+
+button{
+  width: 20%;
+  padding: 1%;
+  margin-right: 3%;
+  background: #181818;
+  color: hsla(160, 100%, 37%, 1);
+  text-decoration: none;
+  border-color: aqua;
+}
+a{
+  color: hsla(160, 100%, 37%, 1);
+  text-decoration: none;
+}
+
+</style>
